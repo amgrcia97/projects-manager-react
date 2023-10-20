39e9cef2507e7337fc6cae0b57e7f054c9663394
Andres Murcia <amgrcia97@gmail.com>
2023-10-20 15:38:31 -0300

    eventos props

--- project-manager/src/components/Form.js
+++ project-manager/src/components/Form.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 function Form() {
 
     function cadastrarUsuario(e) {
-        e.preventDefault()
+        e.preventDefault();
         console.log(`Usuário ${name} foi cadastrado com a senha: ${password}!`)
     }
 
