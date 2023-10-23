b65ced221755f348e4c888dfefe2b6ed4488761b
Andres Murcia <amgrcia97@gmail.com>
2023-10-23 19:53:06 -0300

    message

--- project-manager/src/components/pages/NewProject.js
+++ project-manager/src/components/pages/NewProject.js
@@ -22,7 +22,7 @@ function NewProject() {
     })
       .then((resp) => resp.json())
       .then((data) => {
-        history('/projects', { message: 'Projeto criado com sucesso!' })
+        history('/projects', { state: {message: 'Projeto criado com sucesso!' }})
       })
 
   }
