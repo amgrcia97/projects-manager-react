50a013bdb957f0fe7c8b248d99095dc457396988
Andres Murcia <amgrcia97@gmail.com>
2023-10-23 17:26:06 -0300

    form + API (json-server)

--- project-manager/src/index.js
+++ project-manager/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode> Comentado para evitar o fetch 2X
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
