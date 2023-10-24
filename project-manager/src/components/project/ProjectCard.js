ceddb58b9539622e87440ee50089470e9e489d25
Andres Murcia <amgrcia97@gmail.com>
2023-10-23 21:36:21 -0300

    remoção de projeto

--- project-manager/src/components/project/ProjectCard.js
+++ project-manager/src/components/project/ProjectCard.js
@@ -3,6 +3,11 @@ import styles from './ProjectCard.module.css'
 import {BsPencil, BsFillTrashFill} from 'react-icons/bs'
 
 function ProjectCard({ id, name, budget, category, handleRemove}){
+
+  const remove = (e) => {
+    e.preventDefault()
+    handleRemove(id)
+  }
   return (
     <div className={styles.project_card}>
       <h4>{name}</h4>
@@ -14,7 +19,7 @@ function ProjectCard({ id, name, budget, category, handleRemove}){
       </p>
       <div className={styles.project_card_actions}>
         <Link to="/"><BsPencil/>Editar</Link>
-        <button>
+        <button onClick={remove}>
           <BsFillTrashFill/>Excluir
         </button>
       </div>
